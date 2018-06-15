26308fedc9ad6f437842a45044d753596a11fac7
sjanarth <sjanarth@gmail.com>
2018-06-15 16:30:43 -0700

    add sources

---
+++ src/com/intprep/ListNode.java
@@ -0,0 +1,24 @@
+package com.intprep;
+
+public class ListNode<T> 
+{
+	T value = null;
+	ListNode<T> next = null;
+	
+	public ListNode (T v, ListNode<T> n) {
+		value = v;
+		next = n;
+	}
+	
+	public ListNode (T v){
+		this (v, null);
+	}
+	
+	public T getValue () { return value; }
+	public ListNode<T> getNext () { return next; }
+	public ListNode<T> setNext (ListNode<T> n) { 
+		ListNode<T> n1 = next;
+		next = n;
+		return n1;
+	}
+}
