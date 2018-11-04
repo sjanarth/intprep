73c6559aebed1ecc7bd001c35a30ba00168b0928
Satish Janarthanan <sjanarth@gmail.com>
2018-11-04 15:14:24 -0800

    more sort updates

--- src/com/intprep/sort/AbstractSorter.java
+++ src/com/intprep/sort/AbstractSorter.java
@@ -2,9 +2,29 @@ package com.intprep.sort;
 
 public abstract class AbstractSorter 
 {
+	public void sortMain ()	{
+		int[] input = initRandomArray (10);
+		printArray("Input", input);
+		long start = System.nanoTime();
+		int[] output = sort(input);
+		long end = System.nanoTime();
+		printArray("Output", output);
+		System.out.println(this.getClass().getSimpleName()+" Runtime: "+(end-start));
+		System.out.println();
+	}
+
 	protected abstract int[] sort (int[] input);
 	
+	private int[] initRandomArray (int size) {
+		int[] arr = new int[size];
+		for (int i = 0; i < size; i++)	{
+			arr[i] = (int) (Math.random() * 1000); 
+		}
+		return arr;
+	}
+	
 	protected void debug (int[] input, int i)	{
+		/*
 		System.out.print("(");
 		for (int j = 0; j <= i; j++)	{
 			System.out.print(" "+input[j]);
@@ -14,6 +34,7 @@ public abstract class AbstractSorter
 			System.out.print(" "+input[j]);
 		}
 		System.out.println(" )");
+		*/
 	}
 	
 	protected void printArray (String s, int[] arr) {
--- src/com/intprep/sort/SorterTest.java
+++ src/com/intprep/sort/SorterTest.java
@@ -3,13 +3,13 @@ package com.intprep.sort;
 public class SorterTest 
 {
 	public static void main(String[] args) {
-		int[] input = {53, 4, 77, 46, 23, 888, 93, 1};
-		AbstractSorter sorter = new InsertionSorter ();
-		sorter.printArray("Input", input);
-		long start = System.currentTimeMillis();
-		int[] output = sorter.sort(input);
-		long end = System.currentTimeMillis();
-		sorter.printArray("Output", output);
-		System.out.println("Runtime: "+(end-start));
+		AbstractSorter sorter = new BubbleSorter ();
+		sorter.sortMain();
+		sorter = new InsertionSorter ();
+		sorter.sortMain();
+		sorter = new QuickSorter ();
+		sorter.sortMain();
+		sorter = new SelectionSorter ();
+		sorter.sortMain();
 	}
 }
