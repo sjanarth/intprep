61054a8ff81888c9b98ffcc2e79e707acca2bddd
Satish Janarthanan <sjanarth@gmail.com>
2018-11-23 20:53:11 -0800

    1 more trees problems

---
+++ src/com/intprep/trees/problems/MergeTwoBSTs.java
@@ -0,0 +1,86 @@
+package com.intprep.trees.problems;
+
+public class MergeTwoBSTs 
+{
+    private static class Node {
+        int data;
+        Node left;
+        Node right;
+        public Node (int d)	{ data = d; }
+    }
+
+    private static void traverseInOrder (Node node, StringBuffer sb) {
+    	if (node.left != null)	
+    		traverseInOrder(node.left, sb);
+    	if (sb.length() > 0)
+    		sb.append(","+node.data);
+    	else
+    		sb.append(node.data);
+    	if (node.right != null)	
+    		traverseInOrder(node.right, sb);
+    }
+    
+    private static void printArray (String msg, int[] arr) {
+    	System.out.print(msg);
+    	for (int k : arr)
+    		System.out.print(" "+k);
+    	System.out.println();
+    }
+    
+    private static Node convertToBST (int[] arr, int start, int end) {
+    	if (start > end)
+    		return null;
+    	int mid = (start+end)/2;
+    	//System.out.println("("+start+" "+(mid-1)+") "+mid+" ("+(mid+1)+" "+end+")");
+    	Node root = new Node (arr[mid]);
+		root.left = convertToBST (arr, start, mid-1);
+		root.right = convertToBST (arr, mid+1, end);
+    	return root;
+    }
+    
+    private static int[] bstToArray (Node root) {
+    	StringBuffer sb = new StringBuffer();
+    	traverseInOrder (root, sb);
+    	String[] splits = sb.toString().split(",");
+    	int[] arr = new int[splits.length];
+    	for (int i = 0; i < splits.length; i++)
+    		arr[i] = Integer.parseInt(splits[i]);
+    	return arr;
+    }
+    
+    private static int[] mergeArrays (int[] arr1, int[] arr2) {
+    	int[] merged = new int[arr1.length+arr2.length];
+    	int i = 0, j = 0, k = 0;
+    	while (i < arr1.length && j < arr2.length)	{	
+    		if (arr1[i] < arr2[j])
+    			merged[k++] = arr1[i++];
+    		else
+    			merged[k++] = arr2[j++];
+    	}
+    	while (i < arr1.length) merged[k++] = arr1[i++];
+    	while (j < arr2.length) merged[k++] = arr2[j++];
+    	return merged;
+    }
+    
+    private static Node mergeBSTs (Node root1, Node root2) {
+    	int[] arr1 = bstToArray (root1);
+    	int[] arr2 = bstToArray (root2);
+    	int[] mergedArr = mergeArrays (arr1, arr2);
+    	return convertToBST (mergedArr, 0, mergedArr.length-1);
+    }
+    
+    public static void main (String[] args) {
+    	int[] arr1 = new int[] {30,40,45,50,55,60,70};
+    	printArray ("Input-1", arr1);
+    	Node root1 = convertToBST (arr1, 0, arr1.length-1);
+    	int[] arr2 = new int[] {32,42,47,52,57,62,72};
+    	printArray ("Input-2", arr2);
+    	Node root2 = convertToBST (arr2, 0, arr2.length-1);
+    	Node merged = mergeBSTs (root1, root2);
+    	StringBuffer sb = new StringBuffer();
+    	traverseInOrder (merged, sb);
+    	System.out.println("InOder(merged): "+sb.toString());
+    }
+	
+
+}
