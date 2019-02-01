a38a5ee8d7eb3c731c5098b821b2ea2141d56a18
Satish Janarthanan <sjanarth@gmail.com>
2019-01-31 22:29:42 -0800

    Update AutoComplete

--- src/com/intprep/realqs/wip/AutoComplete.java
+++ src/com/intprep/realqs/wip/AutoComplete.java
@@ -18,16 +18,43 @@ public class AutoComplete
 	public static void main (String[] args)	{
 		Trie trie = buildSampleTrie();
 		
-		List<String> results = trie.searchAlpha("ba");
+		List<String> results = trie.searchAlpha("bu");
 		for (String s : results) System.out.println("  "+s);
+		
+		/*
+		 * There seem to be at least two approaches to updating the frequency of occurrences.
+		 * 
+		 * Approach-1: Bubble up
+		 * 
+		 * 		Every time, updateFreq is called, pass a list of topNodes to the immediate parent.
+		 * 		The parents recursively pass up their top used words, eventually leading to the most 
+		 * 		used words "bubbling up" to the top. While this approach is efficient for small volumes, 
+		 * 		it quickly gets out of control when there are a large number of updateFreq calls.
+		 * 
+		 * Approach-2: Trickle down
+		 * 
+		 * 		After doing all updateFreq calls, call crawl on the root which cascades that recursively
+		 * 		through the entire tree, effectively decoupling the updates to the frequency counters
+		 * 		from the code that determines the top most used words.
+		 * 
+		 * Typically, updates to the trie occur a set period of time. When they do occur, we expect a
+		 * large volume of updates and hence this implementation takes the second (trickle down) approach.
+		 * 
+		 * There are further optimizations possible here. For instance, updateFreq could mark the node as "dirty"
+		 * and the crawl calls could limit themselves to only the dirty nodes. When applied recursively, this
+		 * effectively limits the crawl to only those subtrees of the root that have at least one descendant
+		 * whose frequency has just been updated. 
+		 * 		
+		 */
 		
 		trie.update("ball", 8);
 		trie.update("bath", 4);
-		trie.update("beet", 3);
+		trie.update("beet", 11);
 		//System.out.println(trie.toString());
 		trie.crawl();
-		results = trie.searchTopK("b");
-		for (String s : results) System.out.println("  "+s);
+
+		List<String> results2 = trie.searchTopK("b");
+		for (String s : results2) System.out.println("  "+s);
 	}
 	
 	private static Trie buildSampleTrie() 	{
@@ -36,15 +63,24 @@ public class AutoComplete
 		root.add("barley");
 		root.add("bat");
 		root.add("battle");
+		root.add("batch");
 		root.add("bath");
-		root.add("beet");
 		root.add("beast");
+		root.add("beet");
+		root.add("bell");
 		root.add("best");
 		root.add("book");
 		root.add("boot");
+		root.add("botch");
 		root.add("box");
 		root.add("broad");
 		root.add("bull");
+		root.add("bust");
+		root.add("but");
+		root.add("butt");
+		root.add("butler");
+		root.add("butter");
+		root.add("button");
 		//System.out.println(root.toString());
 		return root;
 	}
@@ -54,10 +90,10 @@ public class AutoComplete
 		public Trie() {
 			root = new TrieNode();
 		}
-		public TrieNode add (String s) {
+		public TrieNode add (String word) {
 			//System.out.println("Trie.add, s="+s);
-			s = s.toLowerCase();
-			Queue<Character> q = toCharQueue(s);
+			word = word.toLowerCase();	// done typically to minimize the overall size of the trie
+			Queue<Character> q = toCharQueue(word);
 			return root.add(q);
 		}
 		public List<String> searchAlpha (String prefix)	{
@@ -66,11 +102,13 @@ public class AutoComplete
 			TrieNode node = root.findNode(q);
 			if (node != null)	{
 				//System.out.println(node.toString(0));
+				/*
 				prefix = prefix.substring(0, prefix.length()-1);
 				List<String> results = new ArrayList<String>();
 				for (String s : node.getAllChildStrings())
 					results.add(prefix+s);
-				return results;
+				*/
+				return node.getAllChildStrings();
 			}
 			return new ArrayList<String>();
 		}
@@ -119,7 +157,7 @@ public class AutoComplete
 			c = ch; 
 			freq = 0;
 			children = new TreeMap<Character,TrieNode>(new NullCharComparator());
-			parentNode = p;
+			parentNode = p;	// a more clever implementation could do away with this
 			topNodes = new TrieNode[DEFAULT_TOP_K];
 			for (int i = 0; i < DEFAULT_TOP_K; i++)
 				topNodes[i] = null;
